8bc5d0be471582c1733baa51e9958b4d752a7b6c
Raphaël Candelier <raphael.candelier@sorbonne-universite.fr>
2023-11-27 18:13:19 +0100

    Computing speed

---
+++ Python/Analysis/Computation/speed.py
@@ -0,0 +1,57 @@
+import os
+import time
+import pandas as pd
+
+import project
+from Network import *
+from  Comparison import *
+import paprint as pa
+
+os.system('clear')
+
+# --- Parameters -----------------------------------------------------------
+
+# l_nA = [10, 20, 50, 100, 200]
+l_nA = [500, 1000]
+
+p = 0.5
+
+nRun = 10
+
+# --------------------------------------------------------------------------
+
+for nA in l_nA:
+
+  # --- Check
+
+  fname = project.root + f'/Files/Speed/nA={nA}.csv'
+
+  pa.line(f'{nRun} tests with nA={nA}')
+
+  # --- Main loop
+
+  
+  t = np.empty(nRun)
+
+  for run in range(nRun):
+
+    Net = Network(nA)
+    Net.set_rand_edges('ER', p)
+    
+    # Net.add_edge_attr('rand', name='test_edge')
+    # Net.add_node_attr('rand', name='test_node')
+
+    Set, Icor = Net.shuffle()
+
+    start = time.process_time()
+    
+    M = matching(Net, Set)
+
+    t[run] = time.process_time() - start
+
+
+  # --- Save
+
+  T = pd.DataFrame()
+  T[0] = t
+  T.to_csv(fname)
---
+++ Python/Analysis/Display/speed.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import project
+
+# os.system('clear')
+
+# === Parameters ===========================================================
+
+l_nA = [10, 20, 50, 100, 200]
+
+# ==========================================================================
+
+t = np.empty(len(l_nA))
+
+for i, nA in enumerate(l_nA):
+
+  fname = project.root + f'/Files/Speed/nA={nA}.csv'
+
+  if os.path.exists(fname):
+
+    # Load data
+    T = pd.read_csv(fname, index_col=0)
+
+    # Compute mean and std
+    m = T.mean()
+    s = T.std()
+  
+    t[i] = m
+
+# --- Display --------------------------------------------------------------
+
+plt.style.use('dark_background')
+
+fig, ax = plt.subplots()
+
+ax.plot(l_nA, t, '.-')
+
+ax.set_xlabel(r'$n_A$')
+ax.set_ylabel(r't (s)')
+
+ax.set_xscale('log')
+ax.set_yscale('log')
+
+plt.show()
