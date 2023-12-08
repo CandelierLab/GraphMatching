35f3c01511872b9fac056830dcbc0ecc73c8d364
Raphaël Candelier <raphael.candelier@sorbonne-universite.fr>
2023-12-08 16:56:27 +0100

    Starting to implement the block procedure

--- Python/Tests/test.py
+++ Python/Tests/test.py
@@ -12,10 +12,10 @@ os.system('clear')
 
 np.random.seed(seed=0)
 
-a = 5
+a = 20
 b = a
 
-p = 0.9
+p = 0.95
 
 # --------------------------------------------------------------------------
 
@@ -32,26 +32,34 @@ sm = np.full(a, None)
 I = np.arange(a)
 J = np.arange(b)
 
-for iter in range(1):
+no_rows = False
+no_cols = False
+
+while True:
 
   # --- Rows
 
   # Indices
   I_ = np.where(np.sum(A, axis=1)==1)[0]
   J_ = [np.where(A[i,:])[0][0] for i in I_]
+  no_rows = len(I_)==0
+  
+  if no_rows and no_cols: break
 
   # Update sure matches
   sm[I[I_]] = J[J_]
 
   # Reduce matrix
   A = np.delete(A, I_, axis=0)
-  A = np.delete(A, J_, axis=1)
+  if A.size:
+    A = np.delete(A, J_, axis=1)
+
+  if not A.size: break
 
   # Reduce index lists
   I = np.delete(I, I_)
   J = np.delete(J, J_)
     
-
   pa.matrix(A)
   print(sm)
   print(I, J)
@@ -61,13 +69,19 @@ for iter in range(1):
   # Indices
   J_ = np.where(np.sum(A, axis=0)==1)[0]
   I_ = [np.where(A[:,j])[0][0] for j in J_]
+  no_cols = len(I_)==0
+
+  if no_rows and no_cols: break
 
   # Update sure matches
   sm[I[I_]] = J[J_]
 
   # Reduce matrix
   A = np.delete(A, I_, axis=0)
-  A = np.delete(A, J_, axis=1)
+  if A.size:
+    A = np.delete(A, J_, axis=1)
+
+  if not A.size: break
 
   # Reduce index lists
   I = np.delete(I, I_)
@@ -77,6 +91,33 @@ for iter in range(1):
   print(sm)
   print(I, J)
 
+pa.matrix(A)
+print(sm)
+print(I, J)
+
 # --- Separate in blocks
 
+pa.line()
 
+# Initialization
+U = np.array([0], dtype=int)
+V = np.array([], dtype=int)
+
+# Break condition
+no_rows = False
+no_cols = False
+
+# Search block members
+while True:
+
+  # Column mates
+  V_ = np.setdiff1d(np.where(np.sum(A[U,:], axis=0))[0], V)
+  no_cols = not V_.size
+  if no_rows and no_cols: break
+  V = np.hstack((V, V_))
+
+  # Row mates
+  U_ = np.setdiff1d(np.where(np.sum(A[:,V], axis=1))[0], U)
+  no_rows = not U_.size
+  if no_rows and no_cols: break
+  U = np.hstack((U, U_))
