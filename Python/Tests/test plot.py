1aa80eec886cf2a7b9d9416c32e4dcfb464df5ee
Raphaël Candelier <raphael.candelier@sorbonne-universite.fr>
2024-01-01 16:19:01 +0100

    Modifying test plot

--- Python/Tests/test plot.py
+++ Python/Tests/test plot.py
@@ -1,4 +1,5 @@
 import os
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,15 +10,14 @@ os.system('clear')
 plt.style.use('dark_background')
 fig, ax = plt.subplots()
 
-A = np.arange(10,100,11)
+x = np.arange(0,10)
+y = [math.factorial(2*n)/math.factorial(n)**2 for n in x]
 
-for a in A:
+ax.plot(x, y, '.-')
 
-  x = np.linspace(0, a, 100)
-  y = (a-x)**x
-
-  ax.plot(x, y, '.-')
-
+# ax.set_xscale('log')
 ax.set_yscale('log')
+
+ax.grid(True)
 
 plt.show()
