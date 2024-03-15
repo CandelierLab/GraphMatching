1511f544e977bfcb455c7db9d63ad60a8284f3cf
Raphaël Candelier <raphael.candelier@sorbonne-universite.fr>
2024-03-15 09:47:38 +0100

    Reorganizing examples

--- Python/Examples/Example.py
+++ Python/Examples/Example.py
@@ -29,5 +29,368 @@ class Example():
 
       case 1.0:
 
+        nA = 4
+        nB = 4
+
+        self.Ga = Network(nA, directed=False)
+        self.Ga.Adj = np.zeros((nA,nA), dtype=bool)
+        self.Ga.Adj[0,1] = True
+        self.Ga.Adj[1,2] = True
+        self.Ga.Adj[2,3] = True
+
+        self.Gb = Network(nB, directed=False)
+        self.Gb.Adj = np.zeros((nB,nB), dtype=bool)
+        self.Gb.Adj[0,1] = True
+        self.Gb.Adj[1,2] = True
+        self.Gb.Adj[2,3] = True
+
+      case 1.1:
+
+        nA = 4
+        nB = 4
+
+        self.Ga = Network(nA, directed=False)
+        self.Ga.Adj = np.zeros((nA,nA), dtype=bool)
+        self.Ga.Adj[0,1] = True
+        self.Ga.Adj[1,2] = True
+        self.Ga.Adj[2,3] = True
+        self.Ga.add_node_attr({'measurable': False, 'values': [0, 1, 0, 0]})
+
+        self.Gb = Network(nB, directed=False)
+        self.Gb.Adj = np.zeros((nB,nB), dtype=bool)
+        self.Gb.Adj[0,1] = True
+        self.Gb.Adj[1,2] = True
+        self.Gb.Adj[2,3] = True
+        self.Gb.add_node_attr({'measurable': False, 'values': [0, 0, 0, 0]})
+
+      case 1.2:
+
+        nA = 4
+        nB = 4
+
+        self.Ga = Network(nA, directed=False)
+        self.Ga.Adj = np.zeros((nA,nA), dtype=bool)
+        self.Ga.Adj[0,1] = True
+        self.Ga.Adj[1,2] = True
+        self.Ga.Adj[2,3] = True
+        self.Ga.add_node_attr({'measurable': False, 'values': [1, 0, 0, 0]})
+
+        self.Gb = Network(nB, directed=False)
+        self.Gb.Adj = np.zeros((nB,nB), dtype=bool)
+        self.Gb.Adj[0,1] = True
+        self.Gb.Adj[1,2] = True
+        self.Gb.Adj[2,3] = True
+        self.Gb.add_node_attr({'measurable': False, 'values': [0, 0, 0, 0]})
+
+      case 1.3:
+
+        nA = 4
+        nB = 4
+
+        self.Ga = Network(nA, directed=False)
+        self.Ga.Adj = np.zeros((nA,nA), dtype=bool)
+        self.Ga.Adj[0,1] = True
+        self.Ga.Adj[1,2] = True
+        self.Ga.Adj[2,3] = True
+        self.Ga.add_node_attr({'measurable': False, 'values': [0, 1, 0, 0]})
+
+        self.Gb = Network(nB, directed=False)
+        self.Gb.Adj = np.zeros((nB,nB), dtype=bool)
+        self.Gb.Adj[0,1] = True
+        self.Gb.Adj[1,2] = True
+        self.Gb.Adj[2,3] = True
+        self.Gb.add_node_attr({'measurable': False, 'values': [0, 1, 0, 0]})
         
+      case 2.0:
+
+        nA = 5
+        nB = 5
+
+        self.Ga = Network(nA)
+        self.Ga.Adj = np.zeros((nA,nA), dtype=bool)
+        self.Ga.Adj[0,1] = True
+        self.Ga.Adj[1,2] = True
+        self.Ga.Adj[0,3] = True
+        self.Ga.Adj[3,4] = True
+        
+        self.Gb = Network(nB)
+        self.Gb.Adj = np.zeros((nB,nB), dtype=bool)
+        self.Gb.Adj[0,1] = True
+        self.Gb.Adj[1,2] = True
+        self.Gb.Adj[0,3] = True
+        self.Gb.Adj[3,4] = True
+        
+      case 2.1:
+
+        nA = 5
+        nB = 5
+
+        self.Ga = Network(nA)
+        self.Ga.Adj = np.zeros((nA,nA), dtype=bool)
+        self.Ga.Adj[0,1] = True
+        self.Ga.Adj[1,2] = True
+        self.Ga.Adj[0,3] = True
+        self.Ga.Adj[3,4] = True
+        self.Ga.add_node_attr({'measurable': False, 'values': [0, 0, 0, 0, 0]})
+
+        self.Gb = Network(nB)
+        self.Gb.Adj = np.zeros((nB,nB), dtype=bool)
+        self.Gb.Adj[0,1] = True
+        self.Gb.Adj[1,2] = True
+        self.Gb.Adj[0,3] = True
+        self.Gb.Adj[3,4] = True
+        self.Gb.add_node_attr({'measurable': False, 'values': [0, 1, 0, 0, 0]})
+
+      case 2.2:
+
+        nA = 5
+        nB = 5
+
+        self.Ga = Network(nA)
+        self.Ga.Adj = np.zeros((nA,nA), dtype=bool)
+        self.Ga.Adj[0,1] = True
+        self.Ga.Adj[1,2] = True
+        self.Ga.Adj[0,3] = True
+        self.Ga.Adj[3,4] = True
+        self.Ga.add_node_attr({'measurable': False, 'values': [0, 1, 0, 0, 0]})
+
+        self.Gb = Network(nB)
+        self.Gb.Adj = np.zeros((nB,nB), dtype=bool)
+        self.Gb.Adj[0,1] = True
+        self.Gb.Adj[1,2] = True
+        self.Gb.Adj[0,3] = True
+        self.Gb.Adj[3,4] = True
+        self.Gb.add_node_attr({'measurable': False, 'values': [0, 1, 0, 0, 0]})
+
+      case 2.3:
+
+        nA = 5
+        nB = 5
+
+        self.Ga = Network(nA)
+        self.Ga.Adj = np.zeros((nA,nA), dtype=bool)
+        self.Ga.Adj[0,1] = True
+        self.Ga.Adj[1,2] = True
+        self.Ga.Adj[0,3] = True
+        self.Ga.Adj[3,4] = True
+
+        self.Gb = Network(nB)
+        self.Gb.Adj = np.zeros((nB,nB), dtype=bool)
+        self.Gb.Adj[0,1] = True
+        self.Gb.Adj[1,2] = True
+        self.Gb.Adj[4,3] = True
+        self.Gb.Adj[3,0] = True
+
+      case 3.0:
+
+        nA = 5
+        nB = 5
+
+        self.Ga = Network(nA)
+        self.Ga.Adj = np.zeros((nA,nA), dtype=bool)
+        self.Ga.Adj[0,1] = True
+        self.Ga.Adj[0,2] = True
+        self.Ga.Adj[0,3] = True
+        self.Ga.Adj[0,4] = True
+
+        self.Gb = Network(nB)
+        self.Gb.Adj = np.zeros((nB,nB), dtype=bool)
+        self.Gb.Adj[0,1] = True
+        self.Gb.Adj[0,2] = True
+        self.Gb.Adj[0,3] = True
+        self.Gb.Adj[0,4] = True
+
+      case 3.1:
+
+        nA = 5
+        nB = 5
+
+        self.Ga = Network(nA)
+        self.Ga.Adj = np.zeros((nA,nA), dtype=bool)
+        self.Ga.Adj[0,1] = True
+        self.Ga.Adj[0,2] = True
+        self.Ga.Adj[0,3] = True
+        self.Ga.Adj[0,4] = True
+        self.Ga.add_node_attr({'measurable': False, 'values': [0, 1, 0, 0, 0]})
+
+        self.Gb = Network(nB)
+        self.Gb.Adj = np.zeros((nB,nB), dtype=bool)
+        self.Gb.Adj[0,1] = True
+        self.Gb.Adj[0,2] = True
+        self.Gb.Adj[0,3] = True
+        self.Gb.Adj[0,4] = True
+        self.Gb.add_node_attr({'measurable': False, 'values': [0, 1, 0, 0, 0]})
+        
+      case 3.2:
+
+        nA = 5
+        nB = 5
+
+        self.Ga = Network(nA)
+        self.Ga.Adj = np.zeros((nA,nA), dtype=bool)
+        self.Ga.Adj[0,1] = True
+        self.Ga.Adj[0,2] = True
+        self.Ga.Adj[0,3] = True
+        self.Ga.Adj[0,4] = True
+        self.Ga.add_node_attr({'measurable': False, 'values': [0, 0, 0, 0, 0]})
+
+        self.Gb = Network(nB)
+        self.Gb.Adj = np.zeros((nB,nB), dtype=bool)
+        self.Gb.Adj[0,1] = True
+        self.Gb.Adj[0,2] = True
+        self.Gb.Adj[0,3] = True
+        self.Gb.Adj[0,4] = True
+        self.Gb.add_node_attr({'measurable': False, 'values': [1, 0, 0, 0, 0]})
+
+      case 4.0:
+
+        nA = 3
+        nB = 5
+
+        self.Ga = Network(nA)
+        self.Ga.Adj = np.zeros((nA,nA), dtype=bool)
+        self.Ga.Adj[0,1] = True
+        self.Ga.Adj[1,2] = True
+        self.Ga.Adj[2,0] = True
+        self.Ga.add_node_attr({'measurable': False, 'values': [1, 0, 0]})
+
+        self.Gb = Network(nB)
+        self.Gb.Adj = np.zeros((nB,nB), dtype=bool)
+        self.Gb.Adj[0,1] = True
+        self.Gb.Adj[1,2] = True
+        self.Gb.Adj[2,3] = True
+        self.Gb.Adj[3,4] = True
+        self.Gb.Adj[4,0] = True
+        self.Gb.add_node_attr({'measurable': False, 'values': [1, 0, 0, 0, 0]})
+
+      case 5.0:
+
+        nA = 5
+        nB = 5
+
+        self.Ga = Network(nA, directed=False)
+        self.Ga.Adj = np.zeros((nA,nA), dtype=bool)
+        self.Ga.Adj[0,2] = True
+        self.Ga.Adj[0,3] = True
+        self.Ga.Adj[0,4] = True
+        self.Ga.Adj[1,2] = True
+        self.Ga.Adj[1,3] = True
+        self.Ga.Adj[1,4] = True
+        self.Ga.Adj[2,4] = True
+        self.Ga.Adj[3,4] = True
+
+        self.Gb = Network(nB, directed=False)
+        self.Gb.Adj = np.zeros((nB,nB), dtype=bool)
+        self.Gb.Adj[0,2] = True
+        self.Gb.Adj[0,3] = True
+        self.Gb.Adj[0,4] = True
+        self.Gb.Adj[1,2] = True
+        self.Gb.Adj[1,3] = True
+        self.Gb.Adj[1,4] = True
+        self.Gb.Adj[2,4] = True
+        self.Gb.Adj[3,4] = True
+
+      case 5.1:
+
+        nA = 5
+        nB = 5
+
+        self.Ga = Network(nA, directed=False)
+        self.Ga.Adj = np.zeros((nA,nA), dtype=bool)
+        self.Ga.Adj[0,2] = True
+        self.Ga.Adj[0,3] = True
+        self.Ga.Adj[0,4] = True
+        self.Ga.Adj[1,2] = True
+        self.Ga.Adj[1,3] = True
+        self.Ga.Adj[1,4] = True
+        self.Ga.Adj[2,4] = True
+        self.Ga.Adj[3,4] = True
+        self.Ga.add_node_attr({'measurable': False, 'values': [1, 0, 0, 0, 0]})
+
+        self.Gb = Network(nB, directed=False)
+        self.Gb.Adj = np.zeros((nB,nB), dtype=bool)
+        self.Gb.Adj[0,2] = True
+        self.Gb.Adj[0,3] = True
+        self.Gb.Adj[0,4] = True
+        self.Gb.Adj[1,2] = True
+        self.Gb.Adj[1,3] = True
+        self.Gb.Adj[1,4] = True
+        self.Gb.Adj[2,4] = True
+        self.Gb.Adj[3,4] = True
+        self.Gb.add_node_attr({'measurable': False, 'values': [1, 0, 0, 0, 0]})
+
+      case 5.2:
+
+        nA = 5
+        nB = 5
+
+        self.Ga = Network(nA, directed=False)
+        self.Ga.Adj = np.zeros((nA,nA), dtype=bool)
+        self.Ga.Adj[0,2] = True
+        self.Ga.Adj[0,3] = True
+        self.Ga.Adj[0,4] = True
+        self.Ga.Adj[1,2] = True
+        self.Ga.Adj[1,3] = True
+        self.Ga.Adj[1,4] = True
+        self.Ga.Adj[2,3] = True
+        self.Ga.Adj[2,4] = True
+        self.Ga.Adj[3,4] = True
+
+        self.Gb = Network(nB, directed=False)
+        self.Gb.Adj = np.zeros((nB,nB), dtype=bool)
+        self.Gb.Adj[0,2] = True
+        self.Gb.Adj[0,3] = True
+        self.Gb.Adj[0,4] = True
+        self.Gb.Adj[1,2] = True
+        self.Gb.Adj[1,3] = True
+        self.Gb.Adj[1,4] = True
+        self.Gb.Adj[2,3] = True
+        self.Gb.Adj[2,4] = True
+        self.Gb.Adj[3,4] = True
+
+      case 6.0:
+
+        nA = 4
+        nB = 4
+
+        self.Ga = Network(nA, directed=False)
+        self.Ga.Adj = np.zeros((nA,nA), dtype=bool)
+        self.Ga.Adj[0,1] = True
+        self.Ga.Adj[1,2] = True
+        self.Ga.Adj[2,3] = True
+        self.Ga.Adj[3,0] = True
+
+        self.Gb = Network(nB, directed=False)
+        self.Gb.Adj = np.zeros((nB,nB), dtype=bool)
+        self.Gb.Adj[0,1] = True
+        self.Gb.Adj[1,2] = True
+        self.Gb.Adj[2,3] = True
+        self.Gb.Adj[3,0] = True
+
+      case 7.0:
+
+        nA = 7
+        nB = 7
+
+        self.Ga = Network(nA)
+        self.Ga.Adj = np.zeros((nA,nA), dtype=bool)
+        self.Ga.Adj[0,1] = True
+        self.Ga.Adj[0,2] = True
+        self.Ga.Adj[0,3] = True
+        self.Ga.Adj[1,4] = True
+        self.Ga.Adj[2,5] = True
+        self.Ga.Adj[3,6] = True
+
+        self.Gb = Network(nB)
+        self.Gb.Adj = np.zeros((nB,nB), dtype=bool)
+        self.Gb.Adj[0,1] = True
+        self.Gb.Adj[0,2] = True
+        self.Gb.Adj[0,3] = True
+        self.Gb.Adj[1,4] = True
+        self.Gb.Adj[2,5] = True
+        self.Gb.Adj[3,6] = True
+
+    # Prepare networks
+    self.Ga.prepare()
+    self.Gb.prepare()
 
--- Python/Examples/tester.py
+++ Python/Examples/tester.py
@@ -1,4 +1,24 @@
 import project
 from Examples.Example import *
 
-T = Example(1.0)
+algo = 'GASM'
+
+Ex = Example(5.0)
+
+# --- Matching
+
+C = Comparison(Ex.Ga, Ex.Gb, algorithm=algo)
+
+# C.compute_scores()
+
+M = C.get_matching(force_perfect=False)
+
+# --- Output
+
+pa.line(os.path.basename(__file__))
+print()
+
+pa.matrix(C.X, title='Node scores')
+# pa.matrix(C.Y)
+
+print(M)
