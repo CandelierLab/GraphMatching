303308eb7078aee93b3690272e81254935b41351
Raphaël Candelier <raphael.candelier@sorbonne-universite.fr>
2024-04-17 22:05:39 +0200

    Fit of gamma at p_star

--- Python/Analysis/Display/Subgraph/gq_ER_delta_p_star.py
+++ Python/Analysis/Display/Subgraph/gq_ER_delta_p_star.py
@@ -3,16 +3,24 @@ import re
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 import project
 
-# os.system('clear')
+os.system('clear')
 
 # === Parameters ===========================================================
 
 dname = project.root + '/Files/Subgraph/delta/'
 
 # ==========================================================================
+
+# --- Fit
+
+# def ffun(x, a, b):
+#   return (1-b)*np.exp(-x/a) + b
+
+bin = np.linspace(0,1,101)
 
 # Regular expression
 p = re.compile("ER_nA=(.*)_nRun=(.*)\.csv")
@@ -30,6 +38,8 @@ for fname in os.listdir(dname):
     nA = int(res.group(1))
     nRun = int(res.group(2))
 
+    if nRun!=1000: continue
+
   # Random matchings
   # ax.axhline(y = 1/nA, color = 'w', linestyle = ':')
 
@@ -44,24 +54,34 @@ for fname in os.listdir(dname):
   m = gamma.mean()
   s = gamma.std()
 
-  # --- Display
+  # --- Fit
 
-  # ax.plot(delta, np.log(m), '.-', label=nA)
-  # ax.axhline(-np.log(nA), color='w', linestyle='--')
+  def ffun(x, a, b):
+    return (1-1/nA)*np.exp(-(x**b)/a) + 1/nA
 
-  g0 = -np.log(nA)
-  ax.plot(delta, -(np.log(m)-g0)/g0  , '.-', label=nA)
+  popt, pcov = curve_fit(ffun, delta, m, p0=(5,1))
+
+  # --- Display ------------------------------------------------------------
+
+  ax.plot(delta, m, '.', label=nA)
+
+  # Fit
+  ax.plot(bin, ffun(bin, *popt), 'w:')
+
+  # ax.axhline(1/nA, color='w', linestyle='--')
+
+  # g0 = -np.log(nA)
+  # ax.plot(delta, -(np.log(m)-g0)/g0  , '.-', label=nA)
   
   
-# --- Misc display settings
+# --- Misc display settings ------------------------------------------------
 
 # ax.set_xscale('log')
 ax.set_yscale('log')
 
-# ax.set_xlabel('$\delta$')
-# ax.set_xlim(0, 1)
-# ax.set_ylabel('accuracy $\gamma$')
-# ax.set_ylim(0, 1.2)
+ax.set_xlabel('$\delta$')
+ax.set_xlim(0, 1)
+ax.set_ylabel('accuracy $\gamma$')
 
 ax.legend()
 
