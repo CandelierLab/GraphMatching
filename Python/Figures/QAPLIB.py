518c0c1ffab3324b7390fbb8bf14f738d324a49e
Raphaël Candelier <raphael.candelier@sorbonne-universite.fr>
2024-09-08 16:24:57 +0200

    Misc modifications

--- Python/Figures/QAPLIB.py
+++ Python/Figures/QAPLIB.py
@@ -19,14 +19,15 @@ import project
 
 l_algo = ['FAQ', '2opt', 'Zager', 'GASM']
 
-lw = 3
-fontsize = 24
+lw = 2
+fontsize = 18
 
 # Colors
 c = {'2opt': '#CC4F1B', 'e2opt': '#FF9848',
      'FAQ': '#FFA500', 'eFAQ': '#FACC2E',
      'Zager': '#1B2ACC', 'eZager': '#089FFF',
-     'GASM': '#3F7F4C', 'eGASM':'#7EFF99'}
+     'GASM': '#3F7F4C', 'eGASM':'#7EFF99',
+     'GASM_GPU': '#000000', 'eGASM_GPU':'#000000'}
 
 # --------------------------------------------------------------------------
 
@@ -42,41 +43,46 @@ if figfile is None:
 
 # --- Load data ------------------------------------------------------------
 
-datapath = project.root + f'/Files/QAPLIB/sgq.csv'
+datapath = project.root + f'/Files/QAPLIB/sgqt.csv'
 
 if os.path.exists(datapath):
 
   # Load data
   data = pd.read_csv(datapath)
 
-print(data.columns)
-
 # --- Computation ----------------------------------------------------------
 
 # --- FAQ
 
-s_FAQ = np.sort(data.s_FAQ/data.s_sol)
-g_FAQ = np.flip(np.sort(data.g_FAQ))
-q_FAQ = np.flip(np.sort(data.q_FAQ))
+s_FAQ = data.s_FAQ/data.s_sol
+# g_FAQ = data.g_FAQ
+# q_FAQ = data.q_FAQ
+t_FAQ = data.t_FAQ
 
 # --- 2opt
 
-s_2opt = np.sort(data.s_2opt/data.s_sol)
-g_2opt = np.flip(np.sort(data.g_2opt))
-q_2opt = np.flip(np.sort(data.q_2opt))
-
+s_2opt = data.s_2opt/data.s_sol
+# g_2opt = data.g_2opt
+# q_2opt = data.q_2opt
+t_2opt = data.t_2opt
 
 # --- Zager
 
-s_Zager = np.sort(data.s_Zager/data.s_sol)
-g_Zager = np.flip(np.sort(data.g_Zager))
-q_Zager = np.flip(np.sort(data.q_Zager))
+s_Zager = data.s_Zager/data.s_sol
+# g_Zager = data.g_Zager
+# q_Zager = data.q_Zager
+t_Zager = data.t_Zager
 
-# --- GASM
+# --- GASM CPU
 
-s_GASM = np.sort(data.s_GASM/data.s_sol)
-g_GASM = np.flip(np.sort(data.g_GASM))
-q_GASM = np.flip(np.sort(data.q_GASM))
+s_GASM = data.s_GASM_CPU/data.s_sol
+# g_GASM = data.g_GASM_CPU
+# q_GASM = data.q_GASM_CPU
+t_GASM_CPU = data.t_GASM_CPU
+
+# --- GASM GPU
+
+t_GASM_GPU = data.t_GASM_GPU
 
 # NaN handling
 s_FAQ[np.isnan(s_FAQ)] = 1
@@ -87,7 +93,7 @@ s_GASM[np.isnan(s_GASM)] = 1
 # --- Display --------------------------------------------------------------
 
 plt.rcParams.update({'font.size': fontsize})
-fig, ax = plt.subplots(1,1, figsize=(10,10))
+fig, ax = plt.subplots(1,2, figsize=(10,5))
 
 # --- Score ratios
 
@@ -95,19 +101,41 @@ cdf_FAQ = ecdf(s_FAQ)
 cdf_2opt = ecdf(s_2opt)
 cdf_Zager = ecdf(s_Zager)
 cdf_GASM = ecdf(s_GASM)
-cdf_FAQ.cdf.plot(ax, color=c['FAQ'], linewidth=lw, label='FAQ')
-cdf_2opt.cdf.plot(ax, color=c['2opt'], linewidth=lw, label='2opt')
-cdf_Zager.cdf.plot(ax, color=c['Zager'], linewidth=lw, label='Zager')
-cdf_GASM.cdf.plot(ax, color=c['GASM'], linewidth=lw, label='GASM')
 
-ax.legend()
-ax.set_xscale('log')
-ax.set_xlim(0.8, 100)
-ax.set_ylim(0, 1)
-ax.set_box_aspect(1)
+cdf_FAQ.cdf.plot(ax[0], color=c['FAQ'], linewidth=lw, label='FAQ')
+cdf_2opt.cdf.plot(ax[0], color=c['2opt'], linewidth=lw, label='2opt')
+cdf_Zager.cdf.plot(ax[0], color=c['Zager'], linewidth=lw, label='Zager')
+cdf_GASM.cdf.plot(ax[0], color=c['GASM'], linewidth=lw, label='GASM')
 
-ax.set_xlabel('ratio')
-ax.set_ylabel('ecdf')
+cdf_FAQ = ecdf(t_FAQ)
+cdf_2opt = ecdf(t_2opt)
+cdf_Zager = ecdf(t_Zager)
+cdf_GASM_CPU = ecdf(t_GASM_CPU)
+cdf_GASM_GPU = ecdf(t_GASM_GPU)
+
+cdf_FAQ.cdf.plot(ax[1], color=c['FAQ'], linewidth=lw, label='FAQ')
+cdf_2opt.cdf.plot(ax[1], color=c['2opt'], linewidth=lw, label='2opt')
+cdf_Zager.cdf.plot(ax[1], color=c['Zager'], linewidth=lw, label='Zager')
+cdf_GASM_CPU.cdf.plot(ax[1], color=c['GASM'], linewidth=lw, label='GASM')
+cdf_GASM_GPU.cdf.plot(ax[1], color=c['GASM_GPU'], linewidth=lw, label='GASM GPU')
+
+ax[0].legend()
+ax[0].set_xscale('log')
+ax[0].set_xlim(0.8, 100)
+ax[0].set_ylim(0, 1)
+ax[0].set_box_aspect(1)
+
+ax[0].set_xlabel('$\phi$')
+ax[0].set_ylabel('ecdf')
+
+ax[1].legend()
+ax[1].set_xscale('log')
+# ax[1].set_xlim(0.8, 100)
+ax[1].set_ylim(0, 1)
+ax[1].set_box_aspect(1)
+
+ax[1].set_xlabel('$t$ (ms)')
+ax[1].set_ylabel('ecdf')
 
 #  --- Output --------------------------------------------------------------
 
